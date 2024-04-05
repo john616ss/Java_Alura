175f97915ceb775f0e41c6538f0f56903b5db172
Juan <dead616ss@gmail.com>
2024-04-05 17:48:17 -0600

    nuevo paquete desafios

---
+++ src/desafios/DesafioUno.java
@@ -0,0 +1,19 @@
+package desafios;
+
+public class DesafioUno {
+	
+	public static void main(String[] args) {
+		
+		double celsius = 30.23;
+		double fahrenheit = (celsius * 1.8) + 32;
+		
+		System.out.println("conversion de temperatura en grados Celsius a Fahrenheit");
+		System.out.println("Grados celsius " +celsius + " a grados Fahrenheit es: "+fahrenheit);
+		System.out.println("Casteando temperatura Fahrenheit");
+		int castFahrenheit = (int)fahrenheit;
+		System.out.println(castFahrenheit);
+		
+		
+	}
+
+}
--- src/primerPrograma/Main.java
+++ src/primerPrograma/Main.java
@@ -1,8 +1,31 @@
 package primerPrograma;
-
+/**
+ * @author John
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("¡Hola Mundo!");
+		//System.out.println("¡Hola Mundo!");
+		System.out.println("Bienvenido(a) a Screen Match");
+		System.out.println("Pelicula: Matrix");
+		int fechaDeLanzamiento = 1999;
+		boolean incluidoEnELPlan = true;
+		double notaDeLaPelicula = 8.2;
+		
+		double media = (8.2 + 6.0 + 9.0)/3;
+		System.out.println(media);
+		
+		String sinopsis = """
+				Matrix es uan paradoja 
+                La mejor pelicula del fin del milenio
+                Fue lanzada en: """ + fechaDeLanzamiento;
+		System.out.println(sinopsis);
+		
+		
+		int clasificacion = (int) media/2;
+		System.out.println(clasificacion);
+		
+		
 	}
 }
