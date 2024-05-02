197df2496553c0fe3ecb384e5449d36430234f3f
Juan <dead616ss@gmail.com>
2024-05-02 14:20:55 -0600

    Updated files

--- src/primerPrograma/Decisiones.java
+++ src/primerPrograma/Decisiones.java
@@ -4,15 +4,23 @@ public class Decisiones {
 
 	public static void main(String[] args) {
 		int fechaDeLanzamiento = 1999;
-		boolean incluidoEnELPlan = true;
+		boolean incluidoEnELPlan = false; //
 		double notaDeLaPelicula = 8.2;
+		String tipoPlan = "Plus";
 		
 		if (fechaDeLanzamiento > 2022) {
 			System.out.println("");
 			System.out.println("Péliculas más populares.");
 		} else {
 			System.out.println("");
-			System.out.println("Péliculas retro que aún vale la pena ver.");
+			System.out.println("Pélicula retro que aún vale la pena ver.");
+		}
+		
+		//tipoPlan == "Plus"
+		if (incluidoEnELPlan || tipoPlan.equals("Plus")) {
+			System.out.println("Disfrute su pelicula");
+		} else {
+			System.out.println("Pelicula no disponible para su plan actual");
 		}
 	}
 }
